927e0170fb48f2c9c51da75edf23fde6208c105b
Shogo Ohta <athos0220@gmail.com>
2021-07-24 23:27:10 +0900

    Annotate each group whether it is a verified one

---
+++ src/clojars_analysis/annotate.clj
@@ -0,0 +1,34 @@
+(ns clojars-analysis.annotate
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [net.cgrand.enlive-html :as html])
+  (:import [java.net URL]))
+
+(defn apparent-domain-name? [group]
+  (nat-int? (str/index-of group \.)))
+
+(defn known-verified-group? [group]
+  (boolean (re-find #"^(?:org|net)\.clojars\." group)))
+
+(defn check-verification [group]
+  (-> (URL. (str "https://clojars.org/groups/" group))
+      html/html-resource
+      (html/select [:span.verified-group])
+      not-empty
+      boolean))
+
+(defn verified? [group]
+  (and (apparent-domain-name? group)
+       (or (known-verified-group? group)
+           (check-verification group))))
+
+(defn annotate-group [group]
+  {:group group :verified (verified? group)})
+
+(defn annotate [{:keys [in out]}]
+  (with-open [r (io/reader in)
+              w (io/writer out)]
+    (binding [*out* w]
+      (doseq [group (line-seq r)]
+        (prn (annotate-group group))
+        (Thread/sleep 1000)))))
