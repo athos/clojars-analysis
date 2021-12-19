5108973decbf7c3ccf6612323c14b3bab07655cc
Shogo Ohta <athos0220@gmail.com>
2021-12-19 15:57:21 +0900

    Change annotation code to adapt recent Clojars update

--- src/clojars_analysis/annotate.clj
+++ src/clojars_analysis/annotate.clj
@@ -1,5 +1,6 @@
 (ns clojars-analysis.annotate
-  (:require [clojure.java.io :as io]
+  (:require [clj-http.client :as http]
+            [clojure.java.io :as io]
             [clojure.string :as str]
             [net.cgrand.enlive-html :as html])
   (:import [java.net URL]))
@@ -10,12 +11,28 @@
 (defn- known-verified-group? [group]
   (boolean (re-find #"^(?:org|net)\.clojars\." group)))
 
+(defn- group-member [group]
+  (let [{:keys [body]} (http/get (str "https://clojars.org/api/groups/" group)
+                                 {:as :json})]
+    (:user (first body))))
+
+(def ^:dynamic *wait-interval* 1000)
+
+(defn- wait []
+  (Thread/sleep *wait-interval*))
+
 (defn- check-verification [group]
-  (-> (URL. (str "https://clojars.org/groups/" group))
-      html/html-resource
-      (html/select [:span.verified-group])
-      not-empty
-      boolean))
+  (let [user (group-member group)
+        _ (wait)
+        verified-groups (-> (URL. (str "https://clojars.org/users/" user))
+                            html/html-resource
+                            (html/select [:div
+                                          (html/has [:> :span.verified-group])
+                                          html/first-child
+                                          html/first-child])
+                            (#(into #{} (map (comp first :content)) %)))]
+    (wait)
+    (contains? verified-groups group)))
 
 (defn- verified? [group]
   (and (apparent-domain-name? group)
@@ -25,10 +42,9 @@
 (defn- annotate-group [group]
   {:group group :verified (verified? group)})
 
-(defn annotate [{:keys [in out]}]
+(defn annotate [{:keys [in out wait-interval] :or {wait-interval 1000}}]
   (with-open [r (io/reader in)
               w (io/writer out)]
-    (binding [*out* w]
+    (binding [*out* w, *wait-interval* wait-interval]
       (doseq [group (line-seq r)]
-        (prn (annotate-group group))
-        (Thread/sleep 1000)))))
+        (prn (annotate-group group))))))
